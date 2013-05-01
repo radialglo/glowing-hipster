c2ef347c970c286aaacf84d1cf53386f58623f23
Anthony Su <antchovee@gmail.com>
2013-05-01 16:16:28 -0700

    * updated .gitignore * moved constants to dependent libraries and constants to proxy-constants.h * added portconf

--- http-proxy.cc
+++ http-proxy.cc
@@ -1,48 +1,7 @@
 /* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
 
-#include <iostream>
-#include "http-request.h"
-#include "http-response.h"
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include  <netdb.h>
-#include <arpa/inet.h>
-
-#include <stdio.h>
-#include <stdlib.h>
-
-
-
-
-#define PORT_NUMBER "16290" // the listening port
-                            // for client connections
-                            // TODO: this number should be reset to 14805
-
-#define REMOTE_CXN_LMT 100 // the number of simulatenous connections
-                           // allowed to remote servers
-
-#define CLIENT_CXN_LMT 10 //the incoming connection limit
-
-#define BACKLOG 2 * CLIENT_CXN_LMT        
-                          //the  number of connections 
-                          //that can be queued on listening  socket 
-                          //we make the backlog a little larger
-                          //to accommadte for more connections
-
-#define PROCESS_LMT CLIENT_CXN_LMT //the number of processes our
-                                   //proxy runs
-
-#define TIMEOUT 15000          //in millseconds  
-                               //the default connection time out of 
-                               // Apache 2.0 httpd
-                               // timeout will be broken into smaller intervals
-                               // to check if connection is still active
-                                   
+#include "proxy-constants.h"
                             
-
-
 
 using namespace std;
 void print_ip_and_port (struct addrinfo **res); 
