79d967eedea359bbf47272acfed99f344e0f6dd3
Stanley Xu <stanley.wux@gmail.com>
2013-05-03 23:00:20 -0700

    Portconf and script change

--- http-tester-conditionalGET.py
+++ http-tester-conditionalGET.py
@@ -99,7 +99,6 @@ try:
     data = resp.read()
     conn.close()
 
-    #time.sleep(3)
     time.sleep(3)
     conn2 = HTTPConnection(proxy)
     conn2.request("GET", "http://127.0.0.1:" + sport1 + "/basic")
